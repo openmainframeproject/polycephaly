d8db0344fc95bda778647513659b4614a57a7c5a
Jerry Edgington <39422650+edge05@users.noreply.github.com>
2020-01-31 13:59:14 -0500

    Update CicsWsBuild.groovy

--- src/main/groovy/com/zos/cics/groovy/utilities/CicsWsBuild.groovy
+++ src/main/groovy/com/zos/cics/groovy/utilities/CicsWsBuild.groovy
@@ -13,6 +13,8 @@ import com.zos.java.utilities.*
  * @author gedgingt
  * @version v4.0.0
  * Date 12/24/2018
+ * SPDX-License-Identifier: Apache-2.0 
+ * SPDX-License-Identifier: CC-BY-4.0
  */
 class CicsWsBuild {
 
