8f62898e60243bb21a524b3e45523bb8810a3cfe
Jerry Edgington <39422650+edge05@users.noreply.github.com>
2020-01-31 14:01:19 -0500

    Update ZosAppBuild.groovy

--- src/main/groovy/com/zos/groovy/utilities/ZosAppBuild.groovy
+++ src/main/groovy/com/zos/groovy/utilities/ZosAppBuild.groovy
@@ -21,6 +21,15 @@ import groovy.time.*
 import groovy.lang.GroovyClassLoader
 import groovy.lang.Script 
 
+/**
+* @author gedgingt
+* @version v4.0.0
+* Date 12/24/2018
+*
+* SPDX-License-Identifier: Apache-2.0 
+* SPDX-License-Identifier: CC-BY-4.0
+*/
+
 class ZosAppBuild {
 	/**
 	 *
