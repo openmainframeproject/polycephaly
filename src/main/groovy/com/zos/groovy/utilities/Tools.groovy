c9b2287d96bc8cfd823464a12794a7315d5b1c67
Jerry Edgington <39422650+edge05@users.noreply.github.com>
2020-01-31 14:00:17 -0500

    Update Tools.groovy

--- src/main/groovy/com/zos/groovy/utilities/Tools.groovy
+++ src/main/groovy/com/zos/groovy/utilities/Tools.groovy
@@ -7,6 +7,15 @@ import com.ibm.dbb.repository.*
 import com.ibm.dbb.dependency.*
 import groovy.io.FileType
 
+/**
+* @author gedgingt
+* @version v4.0.0
+* Date 12/24/2018
+*
+* SPDX-License-Identifier: Apache-2.0 
+* SPDX-License-Identifier: CC-BY-4.0
+*/
+
 class Tools {
 
 	static main(args) {
