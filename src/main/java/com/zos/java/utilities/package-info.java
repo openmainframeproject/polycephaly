3c830c9c0daf7c64fcce34c272876f1bddf8eb61
Jerry Edgington <39422650+edge05@users.noreply.github.com>
2020-01-31 14:06:47 -0500

    Update package-info.java

--- src/main/java/com/zos/java/utilities/package-info.java
+++ src/main/java/com/zos/java/utilities/package-info.java
@@ -2,7 +2,11 @@
  * 
  */
 /**
- * @author ge64297
- * level set 01/18/2018
- */
+* @author gedgingt
+* @version v4.0.0
+* Date 12/24/2018
+*
+* SPDX-License-Identifier: Apache-2.0 
+* SPDX-License-Identifier: CC-BY-4.0
+*/
 package com.zos.java.utilities;
