b4722122048dcaa8e8085857254ee87b7ba861ec
Jerry Edgington <39422650+edge05@users.noreply.github.com>
2020-01-31 14:03:14 -0500

    Update Copybook.groovy

--- src/main/groovy/com/zos/language/Copybook.groovy
+++ src/main/groovy/com/zos/language/Copybook.groovy
@@ -4,7 +4,14 @@ import com.ibm.dbb.repository.*
 import com.ibm.dbb.dependency.*
 import com.ibm.dbb.build.*
 import com.zos.groovy.utilities.*
-
+/**
+* @author gedgingt
+* @version v4.0.0
+* Date 12/24/2018
+*
+* SPDX-License-Identifier: Apache-2.0 
+* SPDX-License-Identifier: CC-BY-4.0
+*/
 class Copybook {
 
 
