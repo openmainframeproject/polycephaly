a03b112d830666d2137e0c1ae206b067b3b4512a
Jerry Edgington <39422650+edge05@users.noreply.github.com>
2020-01-31 14:05:06 -0500

    Update package-info.java

--- src/main/groovy/com/zos/language/package-info.java
+++ src/main/groovy/com/zos/language/package-info.java
@@ -2,7 +2,11 @@
  * 
  */
 /**
- * @author ge64297
- *
- */
+* @author gedgingt
+* @version v4.0.0
+* Date 12/24/2018
+*
+* SPDX-License-Identifier: Apache-2.0 
+* SPDX-License-Identifier: CC-BY-4.0
+*/
 package com.zos.language;
