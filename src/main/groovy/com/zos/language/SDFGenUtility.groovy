bf2b028e11d34c04a49e0758ddf7cee734eb4ec7
Jerry Edgington <39422650+edge05@users.noreply.github.com>
2020-01-31 14:04:52 -0500

    Update SDFGenUtility.groovy

--- src/main/groovy/com/zos/language/SDFGenUtility.groovy
+++ src/main/groovy/com/zos/language/SDFGenUtility.groovy
@@ -10,9 +10,13 @@ import com.zos.groovy.utilities.*
 
 
 /**
- * @author gedgingt
- *
- */
+* @author gedgingt
+* @version v4.0.0
+* Date 12/24/2018
+*
+* SPDX-License-Identifier: Apache-2.0 
+* SPDX-License-Identifier: CC-BY-4.0
+*/
 class SDFGenUtility {
 
 	static main(args) {
