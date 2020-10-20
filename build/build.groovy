875c6b02f99130058822944d73435e56db9b8b11
Jerry Edgington <edge0504@yahoo.com>
2020-10-20 10:21:27 -0400

    added test application build configuration

---
+++ build/build.groovy
@@ -0,0 +1,14 @@
+import com.zos.groovy.utilities.ZosAppBuild
+import com.ibm.dbb.build.BuildProperties
+import com.zos.java.utilities.*
+
+	GroovyObject zBuild = (GroovyObject) ZosAppBuild.newInstance()
+ 	def build = zBuild.execute()
+	
+	 // define local properties
+	 def properties = BuildProperties.getInstance()
+	 
+	 //def subJob = new SubmitJob()
+	 //def subPDS = "${properties.jclPDS}(TESTJOB)"
+	 //subJob.sub("//'$subPDS'")
+	 
