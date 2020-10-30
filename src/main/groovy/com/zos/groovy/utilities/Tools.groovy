817721ec12a6d6b7ca5141cb11be827f0172ef5b
Jerry Edgington <edge0504@yahoo.com>
2020-10-29 20:15:47 -0400

    changed properties.confDir to just confDir

--- src/main/groovy/com/zos/groovy/utilities/Tools.groovy
+++ src/main/groovy/com/zos/groovy/utilities/Tools.groovy
@@ -185,7 +185,7 @@ class Tools {
 			properties.collection = System.getenv(Zconstants.BASENAME).trim()
 		}
 
-		properties.load(new File("$properties.confDir/${properties.collection}.properties"))
+		properties.load(new File("$confDir/${properties.collection}.properties"))
 		properties.buildNodeName = System.getenv(Zconstants.BUILDNAME).trim()
 		if(properties.devHLQ == null) {
 			properties.devHLQ = "${properties.datasetPrefix}.${properties.ProjectName}.${properties.buildNodeName}".toString()
