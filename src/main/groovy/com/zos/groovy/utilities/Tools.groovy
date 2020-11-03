95a9bb4903e0385436a66f89d42ea8c15d854087
Jerry Edgington <edge0504@yahoo.com>
2020-11-02 22:02:48 -0500

    added sorted variable display - maybe

--- src/main/groovy/com/zos/groovy/utilities/Tools.groovy
+++ src/main/groovy/com/zos/groovy/utilities/Tools.groovy
@@ -209,6 +209,16 @@ class Tools {
 			println it
 		}
 		println("************************************************************************************************")
+		Map<String, String> env = System.getenv();
+		LinkedHashMap<String, String> collect =
+				 env.entrySet().stream()
+						 .sorted(Map.Entry.comparingByKey())
+						 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+								 (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+ 
+		 collect.forEach((k, v) -> System.out.println(k + ":" + v));
+		
+		println("************************************************************************************************")
 	
 		if (properties.collection == null) {
 			properties.collection = System.getenv(Zconstants.BASENAME).trim()
