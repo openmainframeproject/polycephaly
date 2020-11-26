92b32e8bfbc724e2e8c8b3a24ab2cf08948b4065
Jerry Edgington <edge0504@yahoo.com>
2020-11-26 07:27:48 -0500

    changed properties.startDate back

--- src/main/groovy/com/zos/program/utilities/ZosAppBuild.groovy
+++ src/main/groovy/com/zos/program/utilities/ZosAppBuild.groovy
@@ -54,7 +54,6 @@ class ZosAppBuild {
 		def opts = tools.parseArgs(executeArgs, usage)
 		if (properties.debug) println("opts = $opts")
 		def properties = tools.loadProperties(opts)
-		properties.startTime = startTime.toString()
 
 		if (!properties.userBuild)
 			tools.validateRequiredProperties(["dbb.RepositoryClient.url", "dbb.RepositoryClient.userId", "password", "collection"])
@@ -69,6 +68,9 @@ class ZosAppBuild {
 		}
 		if (properties.debug) println("******************************************************************************************************")
 		tools.validateRequiredProperties(["BuildList"])
+
+		properties.startTime = startTime.format("yyyyMMdd.hhmmss.mmm")
+		//println("** Build start at $properties.startTime")
 
 		// initialize build artifacts
 		tools.initializeBuildArtifacts()
